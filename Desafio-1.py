0eb466d42e414b008de90567e656605c0f643a31
Thais-Dias12 <thais.dias.n483@gmail.com>
2023-09-13 16:38:10 -0300

    Desafio-1

--- Desafio-1.py
+++ Desafio-1.py
@@ -1,2 +1,10 @@
-#Teste 1
-print()
+casa = float(input('Valor da casa? R$'))
+salario = float(input('Salario do comprador: R$'))
+anos = int(input('Quantos anos de financiamento? '))
+prestação = casa / (anos * 12)
+minimo = salario * 30 / 100
+print('Para pagar uma casa de R${:.0f} em {} anos, a prestação será de R${:.2f}'.format(casa, anos, prestação))
+if prestação <= minimo:
+    print('Emprestimo APROVADO! ')
+else:
+    print('Emprestimo NEGADO! ')
---
+++ Estrutura-If,elif,else.py
@@ -0,0 +1,12 @@
+#Estrutura if, elif e else
+nome = str(input('Qual seu nome? '))
+if nome == 'Thais':
+    print('Que nome bonito!')
+elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
+    print('Seu nome é bem popular no Brasil!')
+elif nome in 'Ana Cláudia Jéssica Juliana':
+    print('Belo nome feminino!')
+else:
+    print('Seu nome é bem normal!')
+    
+print('Tenha um bom dia, {}!'.format(nome))
