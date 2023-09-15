db5149f50b19ca3175bc0d884060b4ab56c81f00
Thais-Dias12 <thais.dias.n483@gmail.com>
2023-09-15 16:21:32 -0300

    Atualização-do-desafio-50-51-e-52

--- Desafio-50.py
+++ Desafio-50.py
@@ -1,3 +1,4 @@
+#Para saber a quantidade de números são pares, e qual a soma entre eles.
 soma = 0
 cont = 0
 for c in range(1, 7):
--- Desafio-51.py
+++ Desafio-51.py
@@ -1,3 +1,5 @@
+#Os termos da PA são identificados pela posição que ocupam na sequência, númerica. 
+#Exemplo: 1, 2, 3, 4, etc
 print('='*21)
 print('10 TERMOS DE UMA PA')
 print('='*21)
@@ -5,5 +7,5 @@ n1 = int(input('Primeiro termo: '))
 razao = int(input('Razão: '))
 decimo = n1 + (10 - 1) * razao
 for c in range(n1, decimo + razao, razao):
-    print('{}'.format(c), end= ' ➞  ')
+    print('{}'.format(c), end= ' ⇾  ')
 print('ACABOU')
