c0ad2ef2d1cc9ffe50cecc2007d3899ca470300a
Thais-Dias12 <thais.dias.n483@gmail.com>
2023-09-23 16:04:28 -0300

    Desafio-65

--- Desafio-65.py
+++ Desafio-65.py
@@ -1,17 +1,17 @@
 resp = 'S'
-soma = quant = média = maior = menor = 0
-while resp == 'Ss':
-    n = int(input('Digite um número: '))
-    soma += n
+soma = quant = media = maior = menor = 0
+while resp in 'Ss':
+    num = int(input('Digite um número: '))
+    soma += num
     quant += 1
     if quant == 1:
-        maior = menor = n
+        maior = menor = num
     else:
-        if n > maior:
-            maior = n
-        if n < menor:
-            menor = n
-    r = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
-média = soma / quant
-print('Você digitou {} números e a média foi {}'.format(quant, média))
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+media = soma / quant
+print('Você digitou {} números e a média foi {}'.format(quant, media))
 print('O maior valor foi {} e o menor foi {}'. format(maior, menor))
