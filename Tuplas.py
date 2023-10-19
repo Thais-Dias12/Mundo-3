dc9303adb49c86b3e582e25dd445c3d039fe80ad
Thais-Dias12 <thais.dias.n483@gmail.com>
2023-10-19 10:08:08 -0300

    Tuplas

--- Tuplas.py
+++ Tuplas.py
@@ -11,3 +11,8 @@ for cont in range(0, len(lanche)):
 #    print(f'Eu vou comer {comida} na posição {pos}')
 
 print('Comi para caramba!')
+
+#.index ele mostra a possição da alguma variavel, mostrando em qual possição ela oculpa
+#.conut conta quantos elementos tem dentro de uma variavel, exemplo lanche. mostrando seu total. exemplo, pudim. ele aparece uma vez. e é assim que o conut vai mostrar.
+#sorted ele coloca em ordem uma lista, ou tuplas.
+#del explui uma variavel.
