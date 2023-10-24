48ebbeea8603a0bd6993b2dc53c5b0dbbf23e121
Thais-Dias12 <thais.dias.n483@gmail.com>
2023-10-24 18:07:58 -0300

    Desafio 77

---
+++ Desafio 77.py
@@ -0,0 +1,16 @@
+# Defina a tupla com as palavras
+palavras = ("python", 
+            "programação", 
+            "Linguagem", 
+            "computador", 
+            "algoritmo")
+
+# Função para encontrar e mostrar as vogais de uma palavra
+def encontrar_vogais(palavras):
+    vogais = [letras for letras in palavras if letras in "aeiou"]
+    return vogais
+
+# Itere sobre as palavras na tupla e mostre suas vogais
+for p in palavras:
+    vogais = encontrar_vogais(p)
+    print(f"Palavra: {p} temos as Vogais: {', '.join(vogais)}")
